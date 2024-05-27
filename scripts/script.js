91fcfa4a45248314546a6e7dff1dcb3d0e52faf5
vitor <vitorrf4@gmail.com>
2024-05-27 13:08:46 -0300

    add features

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+document.getElementById('theme-toggle').addEventListener('click', () => {
+    document.body.classList.toggle('light-theme');
+    document.getElementById('centralized-div').classList.toggle('light-theme');
+});
+
+var currentLang = 'en';
+
+document.getElementById('lang-toggle').addEventListener('click', function() {
+    currentLang = currentLang === 'en' ? 'pt' : 'en';
+    fetch(`languages/${currentLang}.json`)
+        .then(response => response.json())
+        .then(data => {
+            for (var key in data) {
+                document.getElementById(key).textContent = data[key];
+            }
+        });
+});
