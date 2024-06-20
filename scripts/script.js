457952c070f92e9e549ce681744877717d489952
vitor <vitorrf4@gmail.com>
2024-06-20 13:34:12 -0300

    change background

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,18 @@
+let isDarkTheme = true;
+
 document.getElementById('theme-toggle').addEventListener('click', () => {
     document.body.classList.toggle('light-theme');
     document.getElementById('centralized-div').classList.toggle('light-theme');
-    document.getElementById('git-icon').src = "assets/github-dark.png"
+    
+    if (isDarkTheme) {
+        document.body.style.backgroundImage = "url('../assets/white-background.png')";
+        document.getElementById('git-icon').src = "assets/github-dark.png";
+    } else {
+        document.body.style.backgroundImage = "url('../assets/dark-background.png')";
+        document.getElementById('git-icon').src = "assets/github-white.png";
+    }
+    
+    isDarkTheme = !isDarkTheme;
 });
 
 function changeLanguage(language) {
