6529454a67f1de370b51159ff17719b78efd29d7
vitor <vitorrf4@gmail.com>
2024-06-20 16:59:53 -0300

    update texts

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,11 @@
+let language = navigator.language;
+changeLanguage(language);
+
 let isDarkTheme = true;
 
 document.getElementById('theme-toggle').addEventListener('click', () => {
     document.body.classList.toggle('light-theme');
-    document.getElementById('centralized-div').classList.toggle('light-theme');
+    document.getElementsByTagName('main')[0].classList.toggle('light-theme');
     
     if (isDarkTheme) {
         document.body.style.backgroundImage = "url('../assets/white-background.png')";
@@ -16,11 +19,24 @@ document.getElementById('theme-toggle').addEventListener('click', () => {
 });
 
 function changeLanguage(language) {
+    switch (language.toUpperCase()) {
+        case 'PT-BR':case 'PT': case 'PT-PT': language = 'PT-BR'; break;
+        default: language = 'EN-US'; break;
+    }
+    
     fetch(`languages/${language}.json`)
         .then(response => response.json())
         .then(data => {
             for (let key in data) {
-                document.getElementById(key).textContent = data[key];
+                let elements = document.getElementsByClassName(key);
+                for (let e of elements) 
+                    e.textContent = data[key];
             }
         });
 }
+
+// TODO
+// Highlight current selected language button
+// Responsive design
+// Add Kotlin, DSA, and C Web Server projects
+// Better design for the tech stack and projects section 
